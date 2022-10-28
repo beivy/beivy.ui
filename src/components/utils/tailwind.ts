649859b1eced1b6c853a24b04fe9334ec22c1fce
Yongwei <yongwei.dou@gmail.com>
2022-10-28 10:51:17 +0900

    Redefined the tailwind classname types.

---
+++ src/components/utils/tailwind.ts
@@ -0,0 +1,20 @@
+import type { BeTLayout } from '../core/type'
+
+export const twClassName = (() => {
+    return (propName: string, propAttribute: string) => {}
+})()
+export const layout = ({
+    display,
+    position,
+    inset,
+    boxSizing,
+    float,
+    clear,
+    isolation,
+    objectFit,
+    objectPosition,
+    overflow,
+    overscroll,
+    visibility,
+    zIndex,
+}: BeTLayout) => {}
